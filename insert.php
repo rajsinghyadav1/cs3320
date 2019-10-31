deb6c62c56682670ee846ad6abaf6791588ce912
rajsinghyadav1 <55802988+rajsinghyadav1@users.noreply.github.com>
2019-10-31 16:53:48 -0500

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,53 @@
+<html>
+
+<body>
+
+<?php
+
+$con = mysqli_connect("localhost:3306","root","Mahika11","CS3320");
+
+if (!$con)
+
+  {
+
+  die('Could not connect: ' . mysql_error());
+
+  }
+  else echo "connected!!!<br>";
+
+ 
+
+//mysqli_select_db($con);
+//  var_dump( $_POST );
+//  echo $_POST['fName'];
+
+$sql="INSERT INTO CS3320.userInformation (firstName, lastName, address1, address2, city, state, zip, email, notes)
+
+VALUES
+('$_POST[fName]','$_POST[lName]','$_POST[address1]','$_POST[address2]','$_POST[city]','$_POST[state]','$_POST[zip]','$_POST[email]','$_POST[notes]')";
+//('Test','Tester')";
+
+echo $sql;
+
+//execute the INSERT
+if (!mysqli_query($con,$sql))
+
+  {
+
+      die('Error: ' . mysqli_error($con));
+
+  }
+  else {
+      echo "<br>1 record added";
+  }
+ 
+
+mysqli_close($con)
+
+?>
+
+</body>
+
+</html>
+
+ 
